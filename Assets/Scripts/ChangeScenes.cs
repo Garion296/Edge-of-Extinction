af725645cdca465c98f36d54c9476df525a512d8
AnthonyBoggio <a.p.boggio@eagle.clarion.edu>
2018-02-05 17:53:04 -0500

    Add files via upload
    Assets and ProjectSettings

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,16 @@
+﻿//Wil be used whenever a button or something that needs this function will be used to change scenes
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScenes : MonoBehaviour {
+
+    //NO NEED FOR START OR UPDATE FUNCTIONS
+
+    // Changes to the appropriate scene
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
